78315d1fd144f0e20d479fba5d1fe009a59554c3
ahayamb <ahayam.tugas1@gmail.com>
2013-12-30 20:55:10 +0700

    first commit

---
+++ Client.py
@@ -0,0 +1,89 @@
+import socket
+import cPickle
+import thread
+import sys
+import os, msvcrt, time
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect(('localhost', 9999))
+sndObj = {'m' : 'in', 'user' : 'paijox', 'res' : 0}
+clientSocket.sendall(cPickle.dumps(sndObj))
+running = True
+rcvObj = {}
+idNow = 0
+poin = 0
+timeout = 0
+
+def printState(state, cat):
+	os.system('cls')
+	printed = ''
+	if state[0] == ' ': printed += '_ '
+	else : printed += (state[0] + ' ')
+		
+	for i in range(1, len(state)):
+		if state[i] == ' ': printed += '_ '
+		else : printed += (state[i] + ' ')
+		
+	print 'Kategori :', cat
+	print printed
+	print 'Panjang karakter :', len(cat)
+	print 'Sisa waktu :', timeout
+	print '\nPoin :', poin
+
+def inputing(threadName, param):
+	sntObj = {}
+	sntObj['m'] = 'ans'
+	sntObj['ch'] = ''
+	while True:
+		try:
+			a = msvcrt.getch()
+			a = a.capitalize()
+			sntObj['id'] = idNow
+			sntObj['ch'] = a
+			sntObj['state'] = rcvObj['state']
+			clientSocket.sendall(cPickle.dumps(sntObj))
+		except:
+			pass
+
+def updateView(threadName, param):
+	global timeout
+	while True:
+		try:
+			printState(rcvObj['state'], rcvObj['cat'])
+			time.sleep(1)
+			timeout -= 1
+		except:
+			pass
+
+def getQuestion(threadName, param):
+	global running
+	global rcvObj
+	global idNow
+	global poin
+	global timeout
+	while True:
+		try:
+			rcvStr = clientSocket.recv(4096)
+			rcvObj = cPickle.loads(rcvStr)
+			if rcvObj['m'] == 'in' and rcvObj['res'] == 0:
+				clientSocket.close()
+				running = False
+				sys.exit(1)
+			
+			elif rcvObj['m'] == 'quest':
+				idNow = rcvObj['id']
+				timeout = rcvObj['timeout']
+				printState(rcvObj['state'], rcvObj['cat'])
+			
+			elif rcvObj['m'] == 'jud':
+				poin += rcvObj['res']
+				printState(rcvObj['state'], rcvObj['cat'])
+		except:
+			pass
+
+thread.start_new_thread(getQuestion, ('th', 0))
+thread.start_new_thread(inputing, ('th', 0))
+# thread.start_new_thread(updateView, ('th', 0))
+
+while running:
+	pass
---
+++ Server.py
@@ -0,0 +1,158 @@
+import socket
+import thread
+import cPickle
+import select
+import time
+import random
+
+class ServerFIB:
+	def __init__(self):
+		self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.serverSocket.bind(('localhost', 9999))
+		self.serverSocket.listen(100)
+		self.clientProperty = {}
+		self.clientProperty[self.serverSocket] = ['server', 0]
+		self.running = True
+		self.currentWord = ''
+		self.currentWordCat = ''
+		self.wordCat = ['NAMA KOTA', 'NEGARA', 'NAMA BUAH', 'KLUB BOLA']
+		self.wordData = {}
+		self.idNow = 0
+		self.timeout = 3
+		self.initWordBank()
+		
+	def initWordBank(self): 
+		self.wordData[0] = ['ACHEN', 'BOJONEGORO', 'SURABAYA', 'JAKARTA', 'BANDUNG', 'SEMARANG', 'PALEMBANG', 'SERANG', 'TANGERANG', 'MAKASSAR']
+		self.wordData[1] = ['INDONESIA', 'BRAZIL', 'MALAYSIA', 'SINGAPURA', 'THAILAND', 'CHINA', 'ARGENTINA', 'JERMAN', 'BELANDA', 'VENEZUELA']
+		self.wordData[2] = ['APEL', 'NANAS', 'JERUK', 'MANGGA', 'DURIAN', 'ANGGUR', 'SIMALAKAMA', 'BUAH NAGA', 'DELIMA', 'SIRSAK']
+		self.wordData[3] = ['REAL MADRID', 'INTER MILAN', 'BAYERN MUENCHEN', 'CHELSEA', 'ARSENAL', 'MANCHESTER UNITED', 'LIVERPOOL', 'ASTON VILA']
+		
+	
+	def runServer(self):
+		thread.start_new_thread(self.listenClient, ('listenClient', 0))
+		thread.start_new_thread(self.postQuest, ('postQuest', 0))
+		
+		while self.running:
+			pass
+	
+	def listenClient(self, threadName, param):
+		while self.running:
+			try:
+				r, w, e = select.select(self.clientProperty.keys(), [], [])
+				
+#                 print 'Incoming request'
+				for i in r:
+					if i == self.serverSocket:
+						dummyClt, dummyAddr = i.accept()
+						self.clientProperty[dummyClt] = ['client1', 0]
+						print 'success'
+					else:
+						rcvStr = i.recv(4096)
+						rcvObj = cPickle.loads(rcvStr)
+						
+						if rcvObj['m'] == 'in':     # m, user, res
+							exist = False
+							
+							for t in self.clientProperty.values():
+								if t[0] == rcvObj['user']:
+									exist = True
+									break
+							
+							if not exist: 
+								self.clientProperty[i][0] = rcvObj['user']
+								rcvObj['res'] = 1
+								i.sendall(cPickle.dumps(rcvObj))
+								
+								if (self.timeout > 0):
+									sndObj = {}
+									sndObj['m'] = 'quest'
+									sndObj['state'] = ''
+									num = random.randrange(0, len(self.wordCat))
+									self.currentWordCat = self.wordCat[ num ]
+									self.currentWord = self.wordData[ num ][ random.randrange(0, len(self.wordData[num])) ] 
+									sndObj['cat'] = self.currentWordCat
+									for i in range(len(self.currentWord)):
+										sndObj['state'] += ' '
+									
+									sndObj['id'] = self.idNow
+									sndObj['timeout'] = self.timeout
+									i.sendall(cPickle.dumps(sndObj))
+								
+							
+							else:
+								rcvObj['res'] = 0
+								i.sendall(cPickle.dumps(rcvObj))
+								self.clientProperty.pop(i)
+						
+						elif rcvObj['m'] == 'ans':  # m, state, ch, res, id
+							#sndObj : m, res, state
+							sndObj = {}
+							sndObj['m'] = 'jud'
+							sndObj['res'] = 0
+							sndObj['cat'] = self.currentWordCat
+							newState = ''
+							if rcvObj['id'] == self.idNow and rcvObj['state'] != self.currentWord:
+								
+								if rcvObj['state'].find(rcvObj['ch']) == -1:
+									exist = False
+									
+									for x in range(len(self.currentWord)):
+										if self.currentWord[x] == rcvObj['ch']:
+											newState += rcvObj['ch']
+											exist = True
+										else : newState += rcvObj['state'][x]
+									
+									if exist:
+										if newState == self.currentWord: sndObj['res'] = self.timeout
+										else : sndObj['res'] = 1
+									else : sndObj['res'] = -1
+								
+								else : 
+									sndObj['res'] = 0
+									newState = rcvObj['state']
+							
+							else : 
+								sndObj['res'] = 0
+								newState = rcvObj['state']
+							sndObj['state'] = newState
+							i.sendall(cPickle.dumps(sndObj))
+			except:
+				pass
+			
+	def postQuest(self, threadName, param):        
+		while self.running: # m, state, bebas, id, timeout
+			self.timeout = 10
+			
+			#Initialize sent question
+			sndObj = {}
+			sndObj['m'] = 'quest'
+			sndObj['state'] = ''
+			num = random.randrange(0, len(self.wordCat))
+			self.currentWordCat = self.wordCat[ num ]
+			self.currentWord = self.wordData[ num ][ random.randrange(0, len(self.wordData[num])) ] 
+			sndObj['cat'] = self.currentWordCat
+			for i in range(len(self.currentWord)):
+				sndObj['state'] += ' '
+			
+			sndObj['id'] = self.idNow
+			sndObj['timeout'] = self.timeout
+			sndStr = cPickle.dumps(sndObj)
+			
+			# Sending to the clients
+			for i in self.clientProperty.keys():
+				try:
+					if i != self.serverSocket:
+						i.sendall(sndStr)
+				except:
+					print 'error sending to', i
+					self.clientProperty.pop(i)
+					pass
+			
+			while self.timeout > 0:
+				time.sleep(1)
+				self.timeout -= 1
+			
+			self.idNow += 1
+
+myServer = ServerFIB()
+myServer.runServer()
